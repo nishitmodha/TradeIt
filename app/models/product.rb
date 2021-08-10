d140821bd2e2f664eb69831b12cba62d476619f3
nishitmodha <nishitmodha30@gmail.com>
2021-08-10 11:34:00 +0530

    Added Store

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,8 +3,4 @@ class Product < ApplicationRecord
   validates :title, :description, :product_image, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true
-  validates :product_image, allow_blank: true, format: {
-    with: %r{\.(gif|jpg|png)\z}i,
-    message: 'must be a URL for GIF, JPG or PNG image.'
-    }
 end
